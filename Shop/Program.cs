91abe1d98e3dd89c3ee700c19f67fa92fe2a4998
DESKTOP-11KO03I\student <231075@st.yoshida-g.ac.jp>
2024-08-30 10:57:35 +0900

    購入後処理 #4

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -19,8 +19,10 @@ namespace Shop
             bool loop = true;
             int ownGold = 1500;
 
-            List<int> ownItems = new List<int>();
-
+            int[] itemNum = new int[5]
+            {
+                0,0,0,0,0
+            };
             //ハンド番号判定ループ
             while (loop == true)
             {
@@ -30,34 +32,14 @@ namespace Shop
                 Console.WriteLine("==========================================");
                 Console.WriteLine("Merchant：なんでも買ってってくれ！");
                 Console.WriteLine("==========================================");
-                Console.WriteLine("1,いしころ:$1,500");
-                Console.WriteLine("2,パジュラ:$200");
-                Console.WriteLine("3,靴:$100");
-                Console.WriteLine("4,この店の敷物:$398,000");
-                Console.WriteLine("5,お食事券:$500");
+                Console.WriteLine("1,いしころ:$1,500[所有数:" + itemNum[0] + "]");
+                Console.WriteLine("2,パジュラ:$200[所有数:" + itemNum[1] + "]");
+                Console.WriteLine("3,靴:$100[所有数:" + itemNum[2] + "]");
+                Console.WriteLine("4,この店の敷物:$398,000[所有数:" + itemNum[3] + "]");
+                Console.WriteLine("5,お食事券:$500[所有数:" + itemNum[4] + "]");
                 Console.WriteLine("==========================================");
+                
 
-                foreach (int item in ownItems)
-                {
-                    switch (item)
-                    {
-                        case 1: //いしころ
-                            Console.WriteLine("いしころ×" + itemNum[0,1]++);
-                            break;
-                        case 2: //パジュラ
-                            Console.WriteLine("パジュラ×" + itemNum[0, 2]++);
-                            break;
-                        case 3: //靴
-                            Console.WriteLine("靴×" + itemNum[0, 3]++);
-                            break;
-                        case 4: //この店の敷物
-                            Console.WriteLine("この店の敷物×" + itemNum[0, 4]++);
-                            break;
-                        case 5: //お食事券
-                            Console.WriteLine("お食事券×" + itemNum[0, 5]++);
-                            break;
-                    }
-                }
                 Console.Write("\n[所持金:$" + ownGold + "]商品ID(0で退出):");
                 numStr = Console.ReadLine();
 
@@ -79,7 +61,28 @@ namespace Shop
                     if (result != ownGold)
                     {
                         ownGold = result;
-                        ownItems.Add(itemID);
+                        
+                        switch (itemID)
+                        {
+                            case 1: //いしころ
+                                itemNum[0]++;
+                                break;
+                            case 2: //パジュラ
+                                itemNum[1]++;
+                                break;
+                            case 3: //靴
+                                itemNum[2]++;
+                                break;
+                            case 4: //この店の敷物
+                                itemNum[3]++;
+                                break;
+                            case 5: //お食事券
+                                itemNum[4]++;
+                                break;
+
+                            default:
+                                break;
+                        }
                         Console.WriteLine("売れたぞ！！！！");
                         Console.ReadLine();
                     }
