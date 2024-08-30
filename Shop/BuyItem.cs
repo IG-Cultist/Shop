8cd32e237aa33caa633349aa382c20b002b2e43d
DESKTOP-11KO03I\student <231075@st.yoshida-g.ac.jp>
2024-08-30 10:27:57 +0900

    所持金実装 #1 売り物実装 #2 購入処理実装 #3

---
+++ Shop/BuyItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    internal class BuyItem
+    {
+
+        public int Liquidation(int id, int ownGold)
+        {
+            switch (id)
+            {
+                case 1: //いしころ
+                    if (ownGold >= 1500) ownGold -= 1500;
+                    break;
+                case 2: //パジュラ
+                    if (ownGold >= 200) ownGold -= 200;
+                    break;
+                case 3: //靴
+                    if (ownGold >= 100) ownGold -= 100;
+                    break;
+                case 4: //この店の敷物
+                    if (ownGold >= 398000) ownGold -= 398000;
+                    break;
+                case 5: //お食事券
+                    if (ownGold >= 500) ownGold -= 500;
+                    break;
+            }
+
+            return ownGold;
+        }
+    }
+}
---
+++ Shop/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //商品ID指定変数
+            int itemID = 0;
+
+            bool loop = true;
+            int ownGold = 1500;
+
+            List<int> ownItems = new List<int>();
+
+            //ハンド番号判定ループ
+            while (loop == true)
+            {
+                Console.Clear();
+
+                string numStr;
+                Console.WriteLine("==========================================");
+                Console.WriteLine("Merchant：なんでも買ってってくれ！");
+                Console.WriteLine("==========================================");
+                Console.WriteLine("1,いしころ:$1,500");
+                Console.WriteLine("2,パジュラ:$200");
+                Console.WriteLine("3,靴:$100");
+                Console.WriteLine("4,この店の敷物:$398,000");
+                Console.WriteLine("5,お食事券:$500");
+                Console.WriteLine("==========================================");
+
+                foreach (int item in ownItems)
+                {
+                    switch (item)
+                    {
+                        case 1: //いしころ
+                            Console.WriteLine("いしころ×" + itemNum[0,1]++);
+                            break;
+                        case 2: //パジュラ
+                            Console.WriteLine("パジュラ×" + itemNum[0, 2]++);
+                            break;
+                        case 3: //靴
+                            Console.WriteLine("靴×" + itemNum[0, 3]++);
+                            break;
+                        case 4: //この店の敷物
+                            Console.WriteLine("この店の敷物×" + itemNum[0, 4]++);
+                            break;
+                        case 5: //お食事券
+                            Console.WriteLine("お食事券×" + itemNum[0, 5]++);
+                            break;
+                    }
+                }
+                Console.Write("\n[所持金:$" + ownGold + "]商品ID(0で退出):");
+                numStr = Console.ReadLine();
+
+                if (int.TryParse(numStr, out itemID)
+                    && itemID ==0)
+                {
+                    loop = false;
+                    Console.WriteLine("退出");
+                    Console.ReadLine();
+                }
+                    else if (int.TryParse(numStr, out itemID)
+                    && (itemID >= 1 && itemID <= 5))
+                { //数値以外または範囲外の文字列を入力した際、再入力
+                    // 購入処理
+                    BuyItem buyItem = new BuyItem();
+
+                    // 清算結果を代入
+                    int result = buyItem.Liquidation(itemID, ownGold);
+                    if (result != ownGold)
+                    {
+                        ownGold = result;
+                        ownItems.Add(itemID);
+                        Console.WriteLine("売れたぞ！！！！");
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("文無しかい？");
+                        Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("そんなものないよ！");
+                    Console.ReadLine();
+                }
+
+
+            }
+        }
+    }
+}
